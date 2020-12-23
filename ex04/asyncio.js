bcea30d967185d40c3efbcd30023b90ba98d6af2
ywake <ywake@student.42tokyo.jp>
2020-12-23 22:32:19 +0900

    Add: error handling.

--- ex04/asyncio.js
+++ ex04/asyncio.js
@@ -6,7 +6,7 @@
 /*   By: ywake <ywake@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 18:23:44 by ywake             #+#    #+#             */
-/*   Updated: 2020/12/23 18:45:44 by ywake            ###   ########.fr       */
+/*   Updated: 2020/12/23 22:29:51 by ywake            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@ let num = 0;
 if (process.argv.length == 3) {
 	try {
 		fs.readFile(process.argv[2], (err, file) => {
-			for (let i = 0; i < file.length; i++) {
-				const char = file[i];
-				if (char == 10) {
-					num++;
+			if (!err) {
+				for (let i = 0; i < file.length; i++) {
+					const char = file[i];
+					if (char == 10) {
+						num++;
+					}
 				}
+				console.log(num);
 			}
-			console.log(num);
 		});
 	} catch (error) {
 
