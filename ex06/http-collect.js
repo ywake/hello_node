bd52153d4680f4b2cda92ec66179c891b25107d9
ywake <ywake@student.42tokyo.jp>
2020-12-23 22:37:42 +0900

    Impl: ex06

---
+++ ex06/http-collect.js
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   http-collect.js                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ywake <ywake@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/23 18:48:07 by ywake             #+#    #+#             */
+/*   Updated: 2020/12/23 19:33:06 by ywake            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+const http = require("http");
+
+if (process.argv.length == 3)
+{
+	try {
+		http.get(process.argv[2], (res) => {
+			res.setEncoding("utf-8");
+			let rawData = '';
+			res.on('data', (chunk) => {
+				rawData += chunk;
+			});
+			res.on('end', () => {
+				console.log(rawData.length);
+				console.log(rawData);
+			});
+		});
+	} catch (error) {
+
+	}
+}
