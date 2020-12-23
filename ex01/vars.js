4ba88e6d8a4a164feeaa6bf2f54db8b1faa8cc2c
ywake <ywake@student.42tokyo.jp>
2020-12-23 18:22:01 +0900

    Impl: ex00~01

---
+++ ex01/vars.js
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vars.js                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ywake <ywake@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/23 18:02:04 by ywake             #+#    #+#             */
+/*   Updated: 2020/12/23 18:07:08 by ywake            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+let str = "42";
+let num = 42;
+let obj = new Object(42);
+let empty = new Object();
+let bool = true;
+let undef;
+
+console.log(str, "is a", typeof str + ".");
+console.log(num, "is a", typeof num + ".");
+console.log(`${obj}`, "is an", typeof obj + ".");
+console.log(`${empty}`, "is an", typeof empty + ".");
+console.log(bool, "is a", typeof bool + ".");
+console.log(undef, "is an", typeof undef + ".");
