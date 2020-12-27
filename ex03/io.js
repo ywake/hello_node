0885c59d5b68f986e2043c34957fa99071f9a1a6
ywake <ywake@student.42tokyo.jp>
2020-12-27 16:54:45 +0900

    Fix: "utf-8" is missing.

--- ex03/io.js
+++ ex03/io.js
@@ -15,7 +15,7 @@ const fs = require("fs");
 let num = 0;
 if (process.argv.length == 3) {
 	try {
-		let file = fs.readFileSync(process.argv[2], "");
+		let file = fs.readFileSync(process.argv[2], "utf-8");
 		for (let i = 0; i < file.length; i++) {
 			if (file[i] == '\n') {
 				num++;
